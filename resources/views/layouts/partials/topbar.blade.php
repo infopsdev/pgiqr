f77cd03cfa2720918fc57b5858ee13f161050a05
Jesus Tovar <infopsdev@gmail.com>
2026-06-08 11:17:00 -0600

    config: ajustar variables de entorno para produccion en subdirectorio /pgiqr

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -3,8 +3,5 @@
         <div>
             <i class="fa-solid fa-building-user me-1"></i> Departamento de Informática
         </div>
-        <div>
-            <span class="badge bg-secondary">Entorno de Desarrollo</span>
-        </div>
     </div>
 </div>
